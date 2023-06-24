ccacff82f0382eb89799dbe949beb600cd347111
ZeraiGR <kirillgr15@mail.ru>
2023-06-24 22:27:34 +0300

    faq page was implemented

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,22 @@
+import styles from './faq.module.css';
+import Accordion from "@/app/ui/Accordion/Accordion";
+import { data } from "./data";
+import classNames from "classnames";
+import {AccordionGroup} from "@/app/ui/Accordion/ui/AccordionGroup";
+
+export default function Faq () {
+    const defaultGroup = data[0].id;
+
+    return (
+        <section>
+            <h1 className={classNames(styles.title, "title")}>Вопросы-ответы</h1>
+            <Accordion defaultGroup={defaultGroup}>
+                {data.map(({title, text, id}) =>
+                    <AccordionGroup key={id} title={title} id={id}>
+                        {text}
+                    </AccordionGroup>
+                )}
+            </Accordion>
+        </section>
+    )
+}
