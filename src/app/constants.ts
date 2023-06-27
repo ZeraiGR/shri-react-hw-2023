08b133e850f6bdab30bf91d24c2f87328aefbe2d
ZeraiGR <kirillgr15@mail.ru>
2023-06-27 06:39:14 +0300

    update movies list state

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,7 +6,8 @@ export const NO_REVIEWS_ERROR = "Отзывов пока нет. Станьте 
 export const EMPTY_CART_ERROR = "Корзина пуста! Скорее покупайте билеты!";
 export const CINEMAS_LOADING_ERROR = "Произошла ошибка при попытке загрузить доступные киноеатры!";
 export const NO_CINEMAS_ERROR = "На данный момент кинотеатров нет, но скоро они обязательно появятся, не переживайте!";
-export const FATAL_UNKNOWN_ERROR = "Произошла неизвестная ошибка! Пожалуйста, попробуйте позже!"
+export const FATAL_UNKNOWN_ERROR = "Произошла неизвестная ошибка! Пожалуйста, попробуйте позже!";
+export const NOT_FOUND_MOVIES_AFTER_FILTERING = "По данному фильру результатов не нашлось! Попробуйте другую комбинацию!";
 
 export const SPINNER_COLOR = "#FF5500";
 export const HEADER_HEIGHT = 76;
