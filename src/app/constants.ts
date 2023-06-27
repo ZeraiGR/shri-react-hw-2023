be8cf33cc317e7f3306bd8fc408d393410d5d8ad
ZeraiGR <kirillgr15@mail.ru>
2023-06-27 06:17:48 +0300

    constans were implemented

---
+++ src/app/constants.ts
@@ -0,0 +1,15 @@
+export const MOVIES_LOADING_ERROR = "Произошла ошибка при попытке загрузить фильмы, попробуйте позже!";
+export const MOVIE_LOADING_ERROR = "Произошла ошибка при попытке загрузить данный фильм, попробуйте позже!";
+export const NO_MOVIES_ERROR = "На данный момент фильмов нет, но скоро они обязательно появятся";
+export const REVIEWS_LOADING_ERROR = "Не удалось загрузить комментарии!";
+export const NO_REVIEWS_ERROR = "Отзывов пока нет. Станьте первым!"
+export const EMPTY_CART_ERROR = "Корзина пуста! Скорее покупайте билеты!";
+export const CINEMAS_LOADING_ERROR = "Произошла ошибка при попытке загрузить доступные киноеатры!";
+export const NO_CINEMAS_ERROR = "На данный момент кинотеатров нет, но скоро они обязательно появятся, не переживайте!";
+export const FATAL_UNKNOWN_ERROR = "Произошла неизвестная ошибка! Пожалуйста, попробуйте позже!"
+
+export const SPINNER_COLOR = "#FF5500";
+export const HEADER_HEIGHT = 76;
+export const DEFAULT_PADDING_VERTICAL = 24;
+export const DEFAULT_PADDING_HORIZONTAL = 32;
+export const MODAL_ANIMATION_DURATION = 300;
