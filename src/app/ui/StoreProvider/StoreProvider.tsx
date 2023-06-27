b4a476a071da96395c6646436c20cadf3c460acd
ZeraiGR <kirillgr15@mail.ru>
2023-06-27 06:26:50 +0300

    storeProvider was updated

--- src/app/ui/StoreProvider/StoreProvider.tsx
+++ src/app/ui/StoreProvider/StoreProvider.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import React from "react";
-import {Provider} from "react-redux";
-import {store} from "@/store/store";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
-export const StoreProvider = ({children}: {
-    children: React.ReactNode
-}) => {
-    return (
-        <Provider store={store}>{children}</Provider>
-    )
+export function StoreProvider ({children}: { children: React.ReactNode }) {
+    return <Provider store={store}>{children}</Provider>;
 }
